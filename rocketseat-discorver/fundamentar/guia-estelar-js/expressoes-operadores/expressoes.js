0b747c94abd70a84c2fb35067093e8c10b1d9280
Eduardo Moreira <eduluizgm@gmail.com>
2023-01-30 09:44:35 -0300

    expressions and operators

---
+++ rocketseat-discorver/fundamentar/guia-estelar-js/expressoes-operadores/expressoes.js
@@ -0,0 +1,8 @@
+//expressão new
+let name = new String('Edu')
+let age = new Number(18)
+name.surName = "Moreira"
+console.log(name, name.surName, age)
+
+let data = new Date('2023-01-30')
+console.log(data)
