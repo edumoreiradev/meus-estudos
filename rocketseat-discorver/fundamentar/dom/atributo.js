f3a5822b5f813cc223c21ad53f3f64599e029186
Eduardo Moreira <eduluizgm@gmail.com>
2023-02-10 22:24:07 -0300

    ...

---
+++ rocketseat-discorver/fundamentar/dom/atributo.js
@@ -0,0 +1,10 @@
+//Adiconar pegar e remover atributo
+
+const header  = document.querySelector('header')
+header.setAttribute('id', 'header')
+
+const headerID = document.querySelector('#header')
+
+console.log(headerID)
+
+header.setAttribute('class', 'bg header')
---
+++ rocketseat-discorver/fundamentar/dom/input.js
@@ -0,0 +1,7 @@
+//Manipulando conteúdo
+//Mudar o valor de um input
+
+const element = document. querySelector('input')
+
+console.log(element,value)
+element.value = "outro valor"
